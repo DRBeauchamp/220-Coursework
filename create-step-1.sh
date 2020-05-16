62d7f808ec614059adfb84f647f9593b4052f6e1
David Beauchamp <davidrbeauchamp@gmail.com>
2020-05-16 18:57:19 +0100

    Working create and delete a vpc and resources

--- create-step-1.sh
+++ create-step-1.sh
@@ -4,37 +4,39 @@
 RESULTS=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --output json | egrep "VpcId" | cut -f2 -d : | tr -d \",)
 VPC_ID=$RESULTS
 aws ec2 create-tags --resources $VPC_ID --tags Key=Name,Value="Step-1"
+aws ec2 describe-vpcs --vpc-id $VPC_ID
 
 # Create an internet gateway and attach it to the vpc
-aws ec2 create-internet-gateway
-IGW_ID=$(aws ec2 describe-internet-gateways | egrep "igw-" | cut -f2 -d : | tr -d \",)
+RESULTS=$(aws ec2 create-internet-gateway | egrep "igw-" | cut -f2 -d : | tr -d \",)
+IGW_ID=$RESULTS
 aws ec2 attach-internet-gateway --vpc-id $VPC_ID --internet-gateway-id $IGW_ID
 aws ec2 create-tags --resources $IGW_ID --tags Key=Name,Value="Step-1"
+aws ec2 describe-internet-gateways --internet-gateway-id $IGW_ID
 
 # Create a subnet within the vpc and modify it so that new instances within it will be public on launch
-aws ec2 create-subnet --cidr-block 10.0.1.0/24 --vpc-id $VPC_ID
-SUBNET_ID=$(aws ec2 describe-subnets | egrep "SubnetId" | tail -1 | cut -f2 -d : | tr -d \",)
+RESULTS=$(aws ec2 create-subnet --cidr-block 10.0.1.0/24 --vpc-id $VPC_ID | egrep "SubnetId" | cut -f2 -d : | tr -d \",)
+SUBNET_ID=$RESULTS
 aws ec2 modify-subnet-attribute --subnet-id $SUBNET_ID --map-public-ip-on-launch
 aws ec2 create-tags --resources $SUBNET_ID --tags Key=Name,Value="Step-1"
+aws ec2 describe-subnets --subnet-id $SUBNET_ID
 
 # Create a route table in the vpc
-aws ec2 create-route-table --vpc-id $VPC_ID
-RT_ID=$(aws ec2 describe-route-tables | egrep "rtb-" | head -1 | cut -f2 -d : | tr -d \",)
+RESULTS=$(aws ec2 create-route-table --vpc-id $VPC_ID | egrep "rtb-" | cut -f2 -d : | tr -d \",)
+RT_ID=$RESULTS
 aws ec2 create-tags --resources $RT_ID --tags Key=Name,Value="Step-1"
+aws ec2 describe-route-tables --route-table-id $RT_ID
 
-# Create a route within
+# Create a route to the internet gateway and attach table to subnet
 aws ec2 create-route --destination-cidr-block 0.0.0.0/0 --gateway-id $IGW_ID --route-table-id $RT_ID
 aws ec2 associate-route-table --route-table-id $RT_ID --subnet-id $SUBNET_ID
 
 echo "These resources were just created: "
-echo -e $VPC_ID\n $IGW_ID\n $SUBNET_ID\n $RT_ID\n
+echo -e $VPC_ID $IGW_ID $SUBNET_ID $RT_ID
 # $SGroup_ID
+aws ec2 wait vpc-available --vpc-ids $VPC_ID
+echo "Vpc ready, next comes an instance"
 
 
-# aws ec2 describe-vpcs --vpc-ids $VPC_ID --query "State"
-
-
-# NEED TO PUT A TIMER HERE BEFORE STARTING THE INSTANCE
 # In order to create an ec2 instance
 # Create a key pair
 # aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem
--- delete.sh
+++ delete.sh
@@ -5,7 +5,7 @@ VPC_ID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=Step-1" | egrep "
 SUBNET_ID=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" | egrep "SubnetId" | tail -1 | cut -f2 -d : | tr -d \",)
 IGW_ID=$(aws ec2 describe-internet-gateways --filters "Name=attachment.vpc-id,Values=$VPC_ID" | egrep "igw-" | cut -f2 -d : | tr -d \",)
 RT_ID=$(aws ec2 describe-route-tables --filters "Name=association.subnet-id,Values=$SUBNET_ID" | egrep "rtb-" | head -1 | cut -f2 -d : | tr -d \",)
-SGroup_ID=$(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$VPC_ID" | egrep "GroupId" | cut -f2 -d : | tr -d \",)
+# SGroup_ID=$(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$VPC_ID" | egrep "GroupId" | cut -f2 -d : | tr -d \",)
 
 # aws ec2 terminate-instances --instance-ids
 # aws ec2 wait instance-terminated --instance-ids
@@ -14,9 +14,9 @@ SGroup_ID=$(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$VPC_
 # aws ec2 delete-security-group --group-id $SGroup_ID
 
 aws ec2 delete-subnet --subnet-id $SUBNET_ID
-aws ec2 delete-route-table --route-table-id $RT_ID
 aws ec2 detach-internet-gateway --internet-gateway-id $IGW_ID --vpc-id $VPC_ID
 aws ec2 delete-internet-gateway --internet-gateway-id $IGW_ID
+aws ec2 delete-route-table --route-table-id $RT_ID
 aws ec2 delete-vpc --vpc-id $VPC_ID
 
 echo "$VPC_ID $IGW_ID $SUBNET_ID $RT_ID $SGroup_ID have been deleted"
